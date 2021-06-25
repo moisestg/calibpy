c5cccf067ce0a6c52b5179a7085df0324baf7daa
moisestg <moisestg93@gmail.com>
2021-06-25 19:49:37 +0200

    - Add video support (save images to video & display video file) - Resize all image windows to a prespecified screen dimensions

--- calibpy/calibration.py
+++ calibpy/calibration.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Optional, Tuple
 import cv2 as cv
 import numpy as np
 
@@ -9,10 +9,11 @@ LK_PARAMS = dict(
     criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.03))
 
 
-def get_frame(vidcap: cv.VideoCapture, msec: float):
+def get_frame(vidcap: cv.VideoCapture, msec: Optional[float] = None):
     """
     """
-    vidcap.set(cv.CAP_PROP_POS_MSEC, msec)
+    if msec is not None:
+        vidcap.set(cv.CAP_PROP_POS_MSEC, msec)
     has_frames, image = vidcap.read()
     return has_frames, image
 
@@ -46,17 +47,54 @@ def convert_video_to_frames(
     return np.stack(frames)
 
 
-def display_image(image: np.ndarray, points: Optional[np.ndarray] = None,
-                  image_time: float = 0):
+def convert_frames_to_video(
+        out_path: str, images: np.ndarray, points: np.ndarray = None,
+        frame_rate: int = 20):
+    """
+    """
+    wh_tuple = images[0].shape[0:2][::-1]
+    out = cv.VideoWriter(
+        out_path, cv.VideoWriter_fourcc(*'mp4v'), frame_rate, wh_tuple)
+    for idx in range(images.shape[0]):
+        image = images[idx].copy()
+        if points is not None:
+            curr_points = points[idx]
+            for point_idx in range(curr_points.shape[0]):
+                cv.circle(
+                    image, tuple(curr_points[point_idx]), 10, (0, 0, 255), -1)
+        out.write(image)
+    out.release()
+
+
+def display_image(image: np.ndarray, wh: Tuple[int, int],
+                  points: Optional[np.ndarray] = None, image_time: float = 0):
     """ frame_time: in miliseconds
     """
     image = image.copy()
     cv.namedWindow('image', cv.WINDOW_NORMAL)
+    cv.resizeWindow('image', *wh)
     if points is not None:
         for point_idx in range(points.shape[0]):
             cv.circle(image, tuple(points[point_idx]), 10, (0, 0, 255), -1)
     cv.imshow('image', image)
     cv.waitKey(image_time)
+    cv.destroyAllWindows()
+
+
+def display_video(in_path: str, wh: Tuple[int, int]):
+    """
+    """
+    vidcap = cv.VideoCapture(in_path)
+    cv.namedWindow('image', cv.WINDOW_NORMAL)
+    cv.resizeWindow('image', *wh)
+    while vidcap.isOpened():
+        success, image = get_frame(vidcap, msec=None)
+        if not success:
+            break
+        cv.imshow('image', image)
+        if (cv.waitKey(1) & 0xFF) == ord('q'):
+            break
+    vidcap.release()
     cv.destroyAllWindows()
 
 
@@ -69,12 +107,14 @@ def click_point(event, x, y, flags, param):  # param
         points.append(np.array([x, y]))
 
 
-def click_tracking_points(image: np.ndarray) -> np.ndarray:
+def click_tracking_points(
+        image: np.ndarray, wh: Tuple[int, int]) -> np.ndarray:
     """
     """
     image = image.copy()
     points = []
     cv.namedWindow('image', cv.WINDOW_NORMAL)
+    cv.resizeWindow('image', *wh)
     cv.setMouseCallback('image', click_point, [image, points])
 
     while True:
@@ -98,10 +138,10 @@ def track_points_image(
     return points2
 
 
-def track_points_seq(images: np.ndarray):
+def track_points_seq(images: np.ndarray, wh: Tuple[int, int]):
     """
     """
-    initial_points = click_tracking_points(images[0])
+    initial_points = click_tracking_points(images[0], wh)
     initial_points = np.float32(initial_points)
     points = [initial_points]
     prev_points = initial_points
@@ -113,22 +153,3 @@ def track_points_seq(images: np.ndarray):
         prev_points = next_points
         prev_image = curr_image
     return np.int_(np.stack(points))
-
-
-def convert_frames_to_video(
-        out_path: str, images: np.ndarray, points: np.ndarray = None,
-        frame_rate: int = 20):
-    """
-    """
-    wh_tuple = images[0].shape[0:2][::-1]
-    out = cv.VideoWriter(
-        out_path, cv.VideoWriter_fourcc(*'mp4v'), frame_rate, wh_tuple)
-    for idx in range(images.shape[0]):
-        image = images[idx].copy()
-        if points is not None:
-            curr_points = points[idx]
-            for point_idx in range(curr_points.shape[0]):
-                cv.circle(
-                    image, tuple(curr_points[point_idx]), 10, (0, 0, 255), -1)
-        out.write(image)
-    out.release()
